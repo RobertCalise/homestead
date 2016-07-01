c5760e5927970c94d616b50724b20c1218e40d5c
Robert Calise <bob@yec.co>
2016-07-01 13:58:18 -0400

    Use PHP 5.6 instead of PHP 7

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 The official Laravel local development environment.
 
 Official documentation [is located here](http://laravel.com/docs/homestead).
+
+*This fork has been modified to use PHP 5.6 instead of PHP 7*
--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -14,7 +14,7 @@ class Homestead
 
     # Configure The Box
     config.vm.box = settings["box"] ||= "laravel/homestead"
-    config.vm.box_version = settings["version"] ||= ">= 0.4.0"
+    config.vm.box_version = settings["version"] ||= "= 0.3.3"
     config.vm.hostname = settings["hostname"] ||= "homestead"
 
     # Configure A Private Network IP
@@ -29,7 +29,7 @@ class Homestead
 
     # Configure A Few VirtualBox Settings
     config.vm.provider "virtualbox" do |vb|
-      vb.name = settings["name"] ||= "homestead-7"
+      vb.name = settings["name"] ||= "homestead-56"
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
@@ -40,7 +40,7 @@ class Homestead
     # Configure A Few VMware Settings
     ["vmware_fusion", "vmware_workstation"].each do |vmware|
       config.vm.provider vmware do |v|
-        v.vmx["displayName"] = settings["name"] ||= "homestead-7"
+        v.vmx["displayName"] = settings["name"] ||= "homestead-56"
         v.vmx["memsize"] = settings["memory"] ||= 2048
         v.vmx["numvcpus"] = settings["cpus"] ||= 1
         v.vmx["guestOS"] = "ubuntu-64"
@@ -190,7 +190,7 @@ class Homestead
 
     config.vm.provision "shell" do |s|
       s.name = "Restarting Nginx"
-      s.inline = "sudo service nginx restart; sudo service php7.0-fpm restart"
+      s.inline = "sudo service nginx restart; sudo service php5-fpm restart"
     end
 
     # Install MariaDB If Necessary
@@ -227,7 +227,7 @@ class Homestead
     if settings.has_key?("variables")
       settings["variables"].each do |var|
         config.vm.provision "shell" do |s|
-          s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php/7.0/fpm/php-fpm.conf"
+          s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf"
           s.args = [var["key"], var["value"]]
         end
 
@@ -238,7 +238,7 @@ class Homestead
       end
 
       config.vm.provision "shell" do |s|
-        s.inline = "service php7.0-fpm restart"
+        s.inline = "service php5-fpm restart"
       end
     end
 
